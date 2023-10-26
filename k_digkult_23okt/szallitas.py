8081cff7bc4b8f2e6868f5c3a1724b06237bd96c
tigru365 <63838062+tigru365@users.noreply.github.com>
2023-10-26 10:52:30 +0200

    Megoldás: Szállítás (2023.10.25)

---
+++ k_digkult_23okt/szallitas.py
@@ -0,0 +1,36 @@
+""" Szállítás (2023.10.25)
+"""
+
+# 1. feladat
+tomegek = [16, 8, 9, 4, 3, 2, 4, 7, 7, 12, 3, 5, 4, 3, 2]
+# print(f"{tomegek = }")
+
+## A tárgyak tömegének tárolása a szövegfájlból beolvasva
+# from pathlib import Path
+# fajl = Path.cwd() / "forras" / "tomeg.txt"
+# with fajl.open(encoding="ascii") as forrasfajl:
+#     tomegek = list(map(lambda x: int(x.strip()), forrasfajl.readline().split(",")))
+# print(f"{tomegek = }")
+
+# 2. feladat
+print("2. feladat")
+
+print(f"A tárgyak tömegének összege: {sum(tomegek)} kg\n")
+
+# 3. feladat
+print("3. feladat")
+
+MAX_TOMEG = 20
+doboz_tomeg = 0
+dobozok = []
+
+for tomeg in tomegek:
+    if doboz_tomeg + tomeg > MAX_TOMEG:
+        dobozok.append(doboz_tomeg)
+        doboz_tomeg = tomeg
+    else:
+        doboz_tomeg += tomeg
+dobozok.append(doboz_tomeg)
+
+print(f"A dobozok tartalmának tömege (kg): {' '.join(map(str, dobozok))}")
+print(f"A szükséges dobozok száma: {len(dobozok)}")
